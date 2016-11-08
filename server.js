4952746b3653028ceb692f13e96bc4501b87acb0
rahul830ui <rahul830ui@gmail.com>
2016-11-07 19:54:32 -0800

    server file changes

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-var express  = require('express');
+var express = require('express');
 
+// Create our app
 var app = express();
-const port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-   if(req.headers['x-forwarded-proto'] === 'https') {
-       res.redirect('http://' + req.hostname + req.url);
-   } else {
-       next();
-   }
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
 });
+
 app.use(express.static('public'));
 
-app.listen(port, function () {
-    console.log("Express server is up at" + port);
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
